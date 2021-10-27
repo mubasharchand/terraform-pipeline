07f6d4fd2a6056a6a2ad1bd2f73470d128310d96
Mubashar chand <mmubasharchand@gmail.com>
2021-10-27 08:33:42 -0400

    updated

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "azurerm_storage_account" "storage" {
 resource "azurerm_storage_container" "container" {
   name                  = var.storage_container_name
   storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private" 
+  container_access_type = "blob" 
   
 }
 resource "azurerm_storage_blob" "blob" {
