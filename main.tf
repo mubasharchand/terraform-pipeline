9e3f7a9a132d8467060ffefb50ba2d7543af0bd6
Mubashar chand <mmubasharchand@gmail.com>
2021-10-27 08:45:59 -0400

    chanding in acces level

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "azurerm_storage_account" "storage" {
 resource "azurerm_storage_container" "container" {
   name                  = var.storage_container_name
   storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "blob" 
+  container_access_type = "container" 
   
 }
 resource "azurerm_storage_blob" "blob" {
